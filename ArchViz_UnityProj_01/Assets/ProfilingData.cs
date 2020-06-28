97651ff412f37a85445ad7bfe82b3f51183cd79a
Samuel Kreuz <it181518@fhstp.ac.at>
2020-06-28 22:55:03 +0200

    New Unity project

---
+++ ArchViz_UnityProj_01/Assets/ProfilingData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class ProfilingData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Profiler.logFile = "mylog";
+        //Profiler.enableBinaryLog = true;
+        //Profiler.enabled = true;
+
+        StartCoroutine(ProfilingCoroutine());
+    }
+
+    IEnumerator ProfilingCoroutine()
+    {
+        yield return new WaitForSeconds(8);
+        Profiler.enabled = false;
+    }
+
+}
