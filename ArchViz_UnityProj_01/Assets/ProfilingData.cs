22a1d7746259d1c7da8f97fae614870d5e62cfdc
Samuel Kreuz <it181518@fhstp.ac.at>
2020-07-01 20:17:32 +0200

    Substance Plugin added

--- ArchViz_UnityProj_01/Assets/ProfilingData.cs
+++ ArchViz_UnityProj_01/Assets/ProfilingData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Profiling;
 
+
 public class ProfilingData : MonoBehaviour
 {
     // Start is called before the first frame update
