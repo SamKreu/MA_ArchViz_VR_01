df0bcbdeb0d5525067b236f136d25242d149b5c0
Samuel Kreuz <it181518@fhstp.ac.at>
2020-07-19 16:49:02 +0200

    Post Processing inkl. Bloom and White Balance

---
+++ ArchViz_UnityProj_01/Assets/RotationScrips.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScrips : MonoBehaviour
+{
+    int rotationspeed = 1;
+
+    //void start()
+    //{
+        
+    //}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0,rotationspeed,0);
+        rotationspeed += 1;
+    }
+}
